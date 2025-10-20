a22262259f928b681e3f4e51b1e04d81edb6e470
melikegcncodes <240541081@firat.edu.tr>
2025-10-20 21:04:39 +0300

    Update AsciiArtIsim.java

---
+++ TASK-BONUS/AsciiArtIsim.java
@@ -0,0 +1,9 @@
+public class AsciiArtlsim {
+    public static void main(String[] args) {
+    System.out.println("M        M    EEEEEEE    L          İ    K     K    EEEEEEE");
+    System.out.println("MM      MM    E          L               K   KK     E      ");
+    System.out.println("M M MM M M    EEEEE      L          İ    KKKK       EEEEE");
+    System.out.println("M        M    E          L          İ    K   KK     E");
+    System.out.println("M        M    EEEEEEE    LLLLLLL    İ    K     K    EEEEEEE");
+    }
+}
