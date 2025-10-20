7228d4bb5b9eeedf03489733d4f18d659675b441
melikegcncodes <240541081@firat.edu.tr>
2025-10-20 11:55:01 +0300

    Update UcgenAlanHesap.java

---
+++ Task-3-GeometrikHesaplamalar/UcgenAlanHesap.java
@@ -0,0 +1,8 @@
+public class UcgenAlanHesap {
+    public static void main(String[] args) {
+        double a = 3 , b = 4 , c = 5;
+        double s = (a+b+c)/2;
+        double alan = Math.sqrt(s * (s-a) * (s-b) * (s-c));
+        System.out.println("Alan: " + alan);
+    }
+}
