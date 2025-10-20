7d952f269212427a41cb0f0b494b9ccf096adcf6
melikegcncodes <240541081@firat.edu.tr>
2025-10-20 12:55:51 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,18 @@
+public class IsimKarti {
+    public static void main(String[] args) {
+        String ad = "Melike";
+        String soyad = "Gücin";
+        String ogrNo = "240541081";
+        String bolum = "Yazılım Muh.";
+
+        System.out.println("====================");
+        System.out.println(" OGRENCİ BILGILERI ");
+        System.out.println("====================");
+        System.out.println("Ad : " + ad);
+        System.out.println("Soyad : " + soyad);
+        System.out.println("No : " + ogrNo);
+        System.out.println("Bolum : " + bolum);
+        System.out.println("====================");
+
+    }
+}
