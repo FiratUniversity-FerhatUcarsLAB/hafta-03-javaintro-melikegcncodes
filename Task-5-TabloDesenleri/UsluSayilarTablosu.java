3b8e9935a53005e8e2ecd4b05e7ace460261e517
melikegcncodes <240541081@firat.edu.tr>
2025-10-20 16:25:48 +0300

    Update UsluSayilarTablosu.java

---
+++ Task-5-TabloDesenleri/UsluSayilarTablosu.java
@@ -0,0 +1,10 @@
+public class UsluSayılarTablosu {
+    public static void main(String[] args) {
+        System.out.println("a     a^2      a^3");
+        System.out.println("------------------");
+        int a1 = 1;
+        System.out.println(a1 + "       "
+        + (a1 * a1) + "       " + (a1*a1*a1));
+
+    }
+}
