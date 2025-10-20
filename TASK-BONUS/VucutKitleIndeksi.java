7c0fce7a855dc7e3f5a73827652a2e2a09fe0a51
melikegcncodes <240541081@firat.edu.tr>
2025-10-20 22:02:55 +0300

    Update VucutKitleIndeksi.java

---
+++ TASK-BONUS/VucutKitleIndeksi.java
@@ -0,0 +1,13 @@
+public class VucutKitleIndeksi {
+    public static void main(String[] args) {
+
+        double kilo = 70.0;
+        double boy = 1.75;
+
+        double bmi = kilo / (boy * boy);
+
+        System.out.println("Kilo: " + kilo);
+        System.out.println("Boy: " + boy);
+        System.out.println("Vücut Kitle İndeksi (BMI): " + bmi);
+    }
+}
